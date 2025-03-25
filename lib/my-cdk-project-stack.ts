24c00348cf739cf37acfe6a435364c483d33226f
AnkithreddyBureddy <79398702+AnkithreddyBureddy@users.noreply.github.com>
2025-03-25 15:57:02 -0400

    Update my-cdk-project-stack.ts

--- lib/my-cdk-project-stack.ts
+++ lib/my-cdk-project-stack.ts
@@ -1,64 +1,45 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';  // Import Construct from 'constructs'
-import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
-import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
-import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class MyCdkProjectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const sourceOutput = new codepipeline.Artifact();
-    const buildOutput = new codepipeline.Artifact();
+    // S3 Bucket
+    const myBucket = new s3.Bucket(this, 'MyBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev/test
+    });
 
-    // CodeBuild Project
-    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
-      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yml'),
+    // Lambda Function
+    const myLambda = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log('Lambda invoked!');
+          return { statusCode: 200, body: 'Hello from Lambda!' };
+        };
+      `),
       environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        BUCKET_NAME: myBucket.bucketName,
       },
     });
 
-    // CodePipeline
-    new codepipeline.Pipeline(this, 'MyPipeline', {
-      pipelineName: 'MyCdkPipeline',
-      stages: [
-        {
-          stageName: 'Source',
-          actions: [
-            new codepipeline_actions.GitHubSourceAction({
-              actionName: 'GitHub_Source',
-              owner: 'AnkithreddyBureddy',
-              repo: 'My-CDK-Project',
-              oauthToken: cdk.SecretValue.unsafePlainText('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'),
-              output: sourceOutput,
-              branch: 'main',
-            }),
-          ],
-        },
-        {
-          stageName: 'Build',
-          actions: [
-            new codepipeline_actions.CodeBuildAction({
-              actionName: 'CodeBuild',
-              project: buildProject,
-              input: sourceOutput,
-              outputs: [buildOutput],
-            }),
-          ],
-        },
-        {
-          stageName: 'Deploy',
-          actions: [
-            new codepipeline_actions.CloudFormationCreateUpdateStackAction({
-              actionName: 'DeployCDK',
-              stackName: 'MyCdkStack',
-              templatePath: buildOutput.atPath('MyCdkStack.template.json'),
-              adminPermissions: true,
-            }),
-          ],
-        },
-      ],
+    // Grant Lambda access to S3
+    myBucket.grantReadWrite(myLambda);
+
+    // DynamoDB Table
+    const myTable = new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'MyDynamoDBTable',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    // Grant Lambda access to DynamoDB
+    myTable.grantReadWriteData(myLambda);
   }
 }
